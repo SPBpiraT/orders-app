7c755deba013d04e03e593c5fa276728639820f1
SPBpiraT <intake@list.ru>
2026-05-22 02:08:13 +0300

    Prepare project structure

---
+++ src/Orders.Web/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Orders.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(); //Get all
+        }
+        public async Task<IActionResult> Get() //Guid id
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+
+    }
+}
