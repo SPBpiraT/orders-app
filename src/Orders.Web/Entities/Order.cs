4340c80b98161c0e594d3ac07b82aaa0b36be0b0
SPBpiraT <intake@list.ru>
2026-05-23 02:52:22 +0300

    Add app models

---
+++ src/Orders.Web/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace Orders.Web.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public int OrderNum { get; set; }
+        public string SenderCity { get; set; } = string.Empty;
+        public string SenderAddress { get; set; } = string.Empty;
+        public string RecipientCity { get; set; } = string.Empty;
+        public string RecipientAddress { get; set; } = string.Empty;
+        public decimal CargoWeightKg { get; set; }
+        public DateTimeOffset PickDate { get; set; }
+    }
+}
---
+++ src/Orders.Web/Models/OrderViewModels/OrderViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Orders.Web.Models.OrderViewModels
+{
+    public class OrderViewModel
+    {
+        public int OrderNum { get; set; }
+        public string SenderCity { get; set; } = string.Empty;
+        public string SenderAddress { get; set; } = string.Empty;
+        public string RecipientCity { get; set; } = string.Empty;
+        public string RecipientAddress { get; set; } = string.Empty;
+        public decimal CargoWeightKg { get; set; }
+        public DateTimeOffset PickDate { get; set; }
+    }
+}
