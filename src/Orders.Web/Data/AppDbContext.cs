5b0a3fc8f8434b3f08f4910f029d2d76eeb620a3
SPBpiraT <intake@list.ru>
2026-05-23 13:49:13 +0300

    Added DbContext and entity configuration for Order

---
+++ src/Orders.Web/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Orders.Web.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Orders.Web.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- src/Orders.Web/Program.cs
+++ src/Orders.Web/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Orders.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
