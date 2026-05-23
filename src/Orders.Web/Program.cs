60fadb19a815369eda9c1285e0c5af039d54a308
SPBpiraT <intake@list.ru>
2026-05-23 16:18:31 +0300

    Setup database initialization for dev environment

--- src/Orders.Web/Program.cs
+++ src/Orders.Web/Program.cs
@@ -17,6 +17,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
 
 app.UseHttpsRedirection();
 app.UseRouting();
