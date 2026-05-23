0a40923189658f1a484f6f79ad66f34b05d57718
SPBpiraT <intake@list.ru>
2026-05-23 19:07:17 +0300

    Added data seeding for dev environment

--- src/Orders.Web/Data/AppDbContext.cs
+++ src/Orders.Web/Data/AppDbContext.cs
@@ -15,5 +15,33 @@ namespace Orders.Web.Data
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             base.OnModelCreating(builder);
         }
+
+        public void SeedData()
+        {
+            if (Orders.Any()) return;
+
+            Orders.AddRange(
+                new Order
+                {
+                    SenderCity = "Москва",
+                    SenderAddress = "ул. Ленина, д. 10, оф. 5",
+                    RecipientCity = "Санкт-Петербург",
+                    RecipientAddress = "Невский пр., д. 25, кв. 12",
+                    CargoWeightKg = 15.5m,
+                    PickDate = DateTimeOffset.UtcNow
+                },
+                new Order
+                {
+                    SenderCity = "Санкт-Петербург",
+                    SenderAddress = "Невский пр., д. 25, кв. 12",
+                    RecipientCity = "Москва",
+                    RecipientAddress = "ул. Ленина, д. 10, оф. 5",
+                    CargoWeightKg = 25.5m,
+                    PickDate = DateTimeOffset.UtcNow
+                }
+            );
+
+            SaveChanges();
+        }
     }
 }
