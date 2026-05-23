5856d501382967060d9903904df6c3f476be6a56
SPBpiraT <intake@list.ru>
2026-05-23 17:17:56 +0300

    Added range validation for cargo weight in vm

--- src/Orders.Web/Models/OrderViewModels/OrderViewModel.cs
+++ src/Orders.Web/Models/OrderViewModels/OrderViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Orders.Web.Models.OrderViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Web.Models.OrderViewModels
 {
     public class OrderViewModel
     {
@@ -7,6 +9,7 @@
         public string SenderAddress { get; set; } = string.Empty;
         public string RecipientCity { get; set; } = string.Empty;
         public string RecipientAddress { get; set; } = string.Empty;
+        [Range(0.01, 100000.00, ErrorMessage = "Вес груза должен быть больше нуля")]
         public decimal CargoWeightKg { get; set; }
         public DateTimeOffset PickDate { get; set; }
     }
